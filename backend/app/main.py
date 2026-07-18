de26cae51f94370e0ebb3be6db08997c5df4b214
Tetiana Yushkevych <139336268+YshTi@users.noreply.github.com>
2026-07-18 21:02:41 +0200

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -15,7 +15,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
-        "https://notebook-python-fastapi.onrender.com",
+        "https://notebook-python-fast-api.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
